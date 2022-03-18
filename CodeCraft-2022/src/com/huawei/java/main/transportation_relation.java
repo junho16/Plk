d2ecf13670fe7bd03a0cd19d0dc0f9be9e7e14c5
junho16 <nchu17202113@163.com>
2022-03-18 11:05:00 +0800

    feat: 测试第一版

---
+++ CodeCraft-2022/src/com/huawei/java/main/transportation_relation.java
@@ -0,0 +1,12 @@
+package com.huawei.java.main;
+
+/**
+ * @author Junho
+ * @date 2022/3/17 20:30
+ */
+public class transportation_relation {
+	int [] distance ;
+	public transportation_relation(int[] distance) {
+		this.distance = distance;
+	}
+}
